d887a941d0148301c4cbd703df7d11099a09457a
uparikh <uparikh@ncsu.edu>
2018-03-22 18:27:07 -0400

    Update testdb.js

--- checkbox/testdb.js
+++ checkbox/testdb.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 mongoose.connect("mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_IP + ":27017/site?authSource=admin");
 
 const Schema = mongoose.Schema;
-ObjectId=Schema.Types.ObjectId;
+ObjectId = Schema.Types.ObjectId;
 
 const studySchema = new mongoose.Schema({
-     _id: ObjectId,
+    _id: ObjectId,
     name: {
         type: String,
         default: 'Study XYZ'
@@ -89,8 +89,107 @@ instance = new MyModel({
 instance.save(function (err) {
     // if (err) return handleError(err);
     // saved!
+    // mongoose.disconnect()
+});
+
+const votesSchema = new mongoose.Schema({
+    studyId: ObjectId,
+    timestamp: {
+        type: Date,
+        default: new Date()
+    },
+    ip: {
+        type: String,
+        default: '127.0.0.1'
+    },
+    fingerprint: String,
+    answers: {
+        type: [],
+        default: [{
+            "question": 1,
+            "kind": "multichoice",
+            "answer": ["0"]
+        }, {
+            "question": 2,
+            "kind": "singlechoice",
+            "answer": "1"
+        }, {
+            "question": 3,
+            "kind": "singlechoicetable",
+            "answer": {
+                "3_0": "1",
+                "3_1": "2",
+                "3_2": "3",
+                "3_3": "1"
+            }
+        }, {
+            "question": 4,
+            "kind": "multichoicetable",
+            "answer": {
+                "4_0": ["2"],
+                "4_1": ["1"],
+                "4_2": ["2"],
+                "4_3": ["1"]
+            }
+        }]
+    },
+    email: {
+        type: String,
+        default: 'a@b.com'
+    },
+    contact: {
+        type: String,
+        default: 'true'
+    }
+});
+
+const MyModel2 = mongoose.model('vote', votesSchema);
+MyModel2.remove({});
+
+instance = new MyModel2({
+    studyId: mongoose.Types.ObjectId("5aaff0a4857f41071758ec85"),
+    fingerprint: '2481770090'
+});
+
+instance.save(function (err) {
+    // if (err) return handleError(err);
+    // saved!
+    // mongoose.disconnect()
+});
+
+instance = new MyModel2({
+    studyId: mongoose.Types.ObjectId("5aaff0a4857f41071758ec85"),
+    fingerprint: '2481770090'
+});
+
+instance.save(function (err) {
+    // if (err) return handleError(err);
+    // saved!
+    // mongoose.disconnect()
+});
+
+instance = new MyModel2({
+    studyId: mongoose.Types.ObjectId("5aaff0a4857f41071758ec84"),
+    fingerprint: '2481770089'
+});
+
+instance.save(function (err) {
+    // if (err) return handleError(err);
+    // saved!
+    // mongoose.disconnect()
+});
+
+instance = new MyModel2({
+    studyId: mongoose.Types.ObjectId("5aaff0a4857f41071758ec84"),
+    fingerprint: '2481770089'
+});
+
+instance.save(function (err) {
+    // if (err) return handleError(err);
+    // saved!
     mongoose.disconnect()
 });
+
 
 var p11 = {
     "markdown": "\n{NumberQuestions:true}\n-----------\nStart with header for global options:\n\n    {NumberQuestions:true}\n    -----------\n\n### Multiple Choice Question (Check all that apply)\n\nA *description* for question.  \nQuestions are created with headers (level 3) `### Multiple Choice Question (Check all that apply)`.\n\n* Choice A\n* Choice B\n* Choice C\n\n### Single Choice Question\n\nMarkdown is great for including questions about code snippets:\n```\n$(document).ready( function()\n{\n    ko.applyBindings(new TaskViewModel());\n\tvar converter = new Markdown.Converter();\n\tdocument.write(converter.makeHtml(\"**I am bold**\"));\n});\n```\n\n1. Choice\n2. Choice\n3. Choice\n\n### Ranking/Rating Table\n\nThe first column has the description.  [Use github flavored markdown for table formatting](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#wiki-tables).\n\n|                       | Do not Want | Sometimes | Always |\n| --------------------- | ----------- | --------- | ------ | \n| Search terms used in IDE\t                      |  |  |  |\n| Code that did not work out and was deleted.     |  |  |  |\n| Time spent on particular edits\t              |  |  |  |\n| Code and files viewed\t                          |  |  |  |\n\n### Multichoice table\n\nYou can leave the other columns blank, or `[]` if multi choice.\n\n|                 | Read  | Write  |\n| ----------------| ----- | ------ |\n| Blog posts \t  |   []  |  []    |\n| Stack Overflow  |   []  |  []    |\n| Twitter         |   []  |  []    |\n| Github          |   []  |  []    |\n\n### Text multiple line answer\nPlease go into **great** detail, with `> {rows:5}`\n\n&gt; {rows:5}\n\n### Text single line answer\n\nYou can enter in `> {rows:1}` or `> {}`.\n\n&gt; {}\n\n-----------------------\n\n#### Other information\n\nYou can use [json5](https://github.com/aseemk/json5).  _i.e._ `{rows:5}` without quotes `{\"rows\":5}`.\n\nThe recommended method of creating surveys is to edit it in a markdown editor, and then paste it here when you are done.\nThis makes it easier to save and reuse.\n\nThis is a *beta* product, so please give feedback about feature design and requests!\t\n\t",
@@ -149,7 +248,7 @@ var p13 = {
     "studyKind": "survey"
 }
 
-var p1 = [p11,p12,p13];
+var p1 = [p11, p12, p13];
 
 var p21 = {
     "answers": "[{\"question\":1,\"kind\":\"multichoice\",\"answer\":[\"0\"]},{\"question\":2,\"kind\":\"singlechoice\",\"answer\":\"1\"},{\"question\":3,\"kind\":\"singlechoicetable\",\"answer\":{\"3_0\":\"1\",\"3_1\":\"2\",\"3_2\":\"3\",\"3_3\":\"1\"}},{\"question\":4,\"kind\":\"multichoicetable\",\"answer\":{\"4_0\":[\"2\"],\"4_1\":[\"1\"],\"4_2\":[\"2\"],\"4_3\":[\"1\"]}}]",
@@ -166,25 +265,24 @@ var p22 = {
     "studyId": "5aaff0a4857f41071758ec85"
 }
 
-var p2 = [p21,p22];
+var p2 = [p21, p22];
 
 var p3 = {
     "token": "t1"
 }
 
-var arr = ['AMZN','SURFACE','IPADMINI','GITHUB','BROWSERSTACK','NEQ'];
+var arr = ['AMZN', 'SURFACE', 'IPADMINI', 'GITHUB', 'BROWSERSTACK', 'NEQ'];
 var p4 = [];
 
-for(i in arr){
+for (i in arr) {
     p4.push({
         "email": "a@b.com",
         "kind": arr[i]
     });
 }
-console.log(JSON.stringify(p4));
 
-ids = ["5aaff0a4857f41071758ec85","5aaff0a4857f41071758ec84"]
-tokens = ["t1","t2"]
+ids = ["5aaff0a4857f41071758ec85", "5aaff0a4857f41071758ec84"]
+tokens = ["t1", "t2"]
 
 module.exports.ids = ids;
 module.exports.tokens = tokens;
